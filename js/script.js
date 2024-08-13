ed1568ce82de692da51881ebc5e698d2cbf7b3ae
devdxd <devdxd084@gmail.com>
2024-08-13 08:37:33 +0700

    update BMI result comment

--- js/script.js
+++ js/script.js
@@ -1,33 +1,41 @@
 /* ------------------ INTRO ------------------- */
 
+// Mengambil elemen-elemen yang akan digunakan untuk animasi intro
 let intro = document.querySelector('.intro')
-    let logo = document.querySelector('.logo-header')
-    let logoSpan = document.querySelectorAll('.logo')
-    window.addEventListener('DOMContentLoaded',()=>{
-      setTimeout(()=>{
-        logoSpan.forEach((span,idx) => {
-          setTimeout(()=>{
-            span.classList.add('active')
-        }, (idx+1)*400);
-      });
+let logo = document.querySelector('.logo-header')
+let logoSpan = document.querySelectorAll('.logo')
 
+// Menambahkan event listener saat seluruh konten DOM telah dimuat
+window.addEventListener('DOMContentLoaded',()=>{
+  setTimeout(()=>{
+    // Menambahkan class 'active' pada setiap elemen logo secara bertahap
+    logoSpan.forEach((span,idx) => {
       setTimeout(()=>{
-        logoSpan.forEach((span,idx)=>{
-          setTimeout(()=>{
-            span.classList.remove('active');
-            span.classList.add('fade');
-          }, (idx+1)*50)
-        })
-      },2000);
-      setTimeout(() => {
-        intro.style.top= '-100vh'
-      },2300);
-    })
+        span.classList.add('active')
+      }, (idx+1)*400);
+    });
+
+    // Menghapus class 'active' dan menambahkan class 'fade' setelah interval waktu tertentu
+    setTimeout(()=>{
+      logoSpan.forEach((span,idx)=>{
+        setTimeout(()=>{
+          span.classList.remove('active');
+          span.classList.add('fade');
+        }, (idx+1)*50)
+      })
+    },2000);
+
+    // Menggeser elemen intro keluar layar setelah animasi selesai
+    setTimeout(() => {
+      intro.style.top= '-100vh'
+    },2300);
   })
+})
 
 
 /* ------------------ HITUNG BMI ------------------- */
 
+// Mengambil elemen-elemen input dan hasil yang akan digunakan untuk kalkulasi BMI
 var weight = document.getElementById("weight");
 var height = document.getElementById("height");
 var age = document.getElementById("age");
@@ -44,21 +52,25 @@ let sugestHigh = document.querySelector(".sugest-high");
 let sugestVeryHigh = document.querySelector(".sugest-veryhigh");
 let sugestText = document.querySelector(".sugest-text");
 
+//Mengecek apakah semua kolom di Form sdh terisi atau belum
 function calculate(){
  
-    if(age.value=='' || height.value=='' || weight.value=='' || (male.checked==false && female.checked==false)){
-      popup.style.display = "block";
-      popupText.innerHTML = `Harap isi semua kolom!`;
+  if(age.value=='' || height.value=='' || weight.value=='' || (male.checked==false && female.checked==false)){
+    popup.style.display = "block";
+    popupText.innerHTML = `Harap isi semua kolom!`;
   
-    }else{
-      calcBmi();
-    }
-  
+  }else{
+    calcBmi(); // Memanggil fungsi untuk menghitung BMI
   }
+  
+}
 
+//Fungsi untuk menghitung nilai BMI
 function calcBmi() {
 
+    // Membuat array `p` yang berisi nilai input dari berat badan, tinggi badan, dan usia
     var p = [weight.value, height.value, age.value];
+    // Mengecek jenis kelamin yang dipilih dan menambahkannya ke dalam array `p`
     if (male.checked) {
         p.push("male");
     }
@@ -66,9 +78,12 @@ function calcBmi() {
         p.push("female");
     }
 
+    // Menghitung nilai BMI berdasarkan berat[0] dan tinggi[1]
     var bmi = Number(p[0])/(Number(p[1])/100*Number(p[1])/100);
 
     var result = '';
+
+    // Mengecek kategori BMI dan menampilkan hasil serta saran yang sesuai
     if (bmi < 18.5) {
         result = 'Kekurangan berat badan';
         sugestLow.style.display = "block";
@@ -115,7 +130,7 @@ function calcBmi() {
         document.querySelector(".sugest-text").innerHTML = `Hasil BMI Kamu menunjukkan bahwa Kamu berada dalam kategori obesitas ekstrim. Kondisi ini sangat berisiko dan memerlukan perhatian medis segera. Disarankan untuk berkonsultasi dengan dokter untuk rencana penurunan berat badan yang aman dan efektif.`;
     }
 
-    
+    // Menampilkan hasil BMI dan saran
     textInfo.style.display = "none";
     resultText.style.display = "block";
     dlBtnResult.style.display = "block";
@@ -127,17 +142,18 @@ function calcBmi() {
 
 /* ------------------ POPUP ------------------- */
 
+// Mengambil elemen popup dan tombol close (x)
 popupContent = document.querySelector(".popup-content");
 popupText = document.querySelector("#popupText");
 var popup = document.getElementById("mypopup");
 var span = document.getElementsByClassName("close")[0];
 
-// Jika klik <span> (x), tutup popup
+// Menutup popup saat tombol close (x) diklik
 span.onclick = function() {
     popup.style.display = "none";
 }
   
-// Jika klik dimanapun diluar popup, tutup popup
+// Menutup popup jika mengklik di luar popup
 window.onclick = function(event) {
     if (event.target == popup) {
         popup.style.display = "none";
